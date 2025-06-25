4586d3f31b010e28032a25e28823f02c876dd5ce
hidaka@HOME <stern.komo@gmail.com>
2025-06-25 18:32:27 +0900

    実験完了

--- src/exec0.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-
-mpirun -np 4 -machinefile machinefile.txt ./a_vector.out ../data/ELSES_MATRIX_VCNT40000std_A.mtx 0
-echo "--------------------"
-mpirun -np 4 -machinefile machinefile.txt ./a_shift.out ../data/ELSES_MATRIX_VCNT40000std_A.mtx 0
-echo "--------------------"
-./a_normal.out ../data/ELSES_MATRIX_VCNT40000std_A.mtx 0
--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-./exec0.sh > ret0.txt
-./exec1.sh > ret1.txt
-./exec1.sh > ret2.txt
+# VCNT4000std
+./exec.sh ../data/ELSES_MATRIX_VCNT4000std_A.mtx 0 > ret-VCNT4000std-0.txt
+./exec.sh ../data/ELSES_MATRIX_VCNT4000std_A.mtx 1 > ret-VCNT4000std-1.txt
+./exec.sh ../data/ELSES_MATRIX_VCNT4000std_A.mtx 2 > ret-VCNT4000std-2.txt
+
+# VCNT40000std
+./exec.sh ../data/ELSES_MATRIX_VCNT40000std_A.mtx 0 > ret-VCNT40000std-0.txt
+./exec.sh ../data/ELSES_MATRIX_VCNT40000std_A.mtx 1 > ret-VCNT40000std-1.txt
+./exec.sh ../data/ELSES_MATRIX_VCNT40000std_A.mtx 2 > ret-VCNT40000std-2.txt
+
+
+# TODO
+# ./a.out $1 $2 > ret$2_$1.txt
+# のような書き方もできる
